f6ce1dec3ba0020022c43eec94fa00ec41f48d72
maibairun <maibairun@163.com>
2017-02-12 11:44:21 +0800

    测试一下

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <section id="primary" class="content-area">
-    <div class="gg">吴婉昕是个大傻瓜</div>
+    <div class="gg">吴婉昕是个大傻瓜哈哈哈</div>
 </section><!-- .content-area -->
 
 <?php get_footer(); ?>
