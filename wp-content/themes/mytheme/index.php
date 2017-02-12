88616262033063410b38eda9f39e1e84f3a3f5e8
maibairun <maibairun@163.com>
2017-02-12 11:35:57 +0800

    测试一下

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <section id="primary" class="content-area">
-    <div>吴婉昕是个大傻瓜</div>
+    <div class="xinxin">吴婉昕是个大傻瓜</div>
 </section><!-- .content-area -->
 
 <?php get_footer(); ?>
